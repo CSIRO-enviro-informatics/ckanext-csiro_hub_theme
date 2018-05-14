1c6f0cd40c1c34cdd1a57adde810a9a40d7f5307
Ashley Sommer <ashleysommer@gmail.com>
2018-05-14 13:20:09 +1000

    Port some parts of template to Bootstrap3. Add logic to plugin to import bs3 templates if required.

--- ckanext/csiro_hub_theme/plugin.py
+++ ckanext/csiro_hub_theme/plugin.py
@@ -1,6 +1,9 @@
+from ckan import __version__ as ckan__version__
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
+from util import version
 
+ckan_version = version.parse(ckan__version__)
 
 class Csiro_Hub_ThemePlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
@@ -8,6 +11,12 @@ class Csiro_Hub_ThemePlugin(plugins.SingletonPlugin):
     # IConfigurer
 
     def update_config(self, config_):
+        # first defined templates are higher priority
+        if ckan_version < version.parse("2.8.0"):
+            # override some parts with bootstrap2 templates if needed
+            toolkit.add_template_directory(config_, 'templates-bs2')
+        # fallback to Bootstrap3 templates.
         toolkit.add_template_directory(config_, 'templates')
+
         toolkit.add_public_directory(config_, 'public')
         toolkit.add_resource('fanstatic', 'csiro_hub_theme')
