764b2c934021f2aa8d4bb9c7f5dc944925f286ff
Jonathan Yu <jonathan.yu@csiro.au>
2018-10-15 21:52:39 +0000

    working kn cross-search

--- ckanext/csiro_hub_theme/fanstatic/csirohubtheme_search.js
+++ ckanext/csiro_hub_theme/fanstatic/csirohubtheme_search.js
@@ -13,7 +13,19 @@ ckan.module('csirohubtheme_search_kn', function ($) {
             console.log("this.options");
             console.log(this.options);
 
-            $(.kn-search-results).append("hello");
+            var query = this.options.query;
+            var params = {
+               q: query
+            }
+
+
+	    $.getJSON( "https://es.knowledgenet.co/datasets32/_search", params, function( data ) {
+                console.log(data);
+                var num_hits = data.hits.total;
+                if(num_hits && num_hits > 0) {
+                   $('.kn-search-results').append("<div class='kn-search-result-hits'>" +num_hits + " datasets found at <a href='http://knowledgenet.co/search/" + query + "'>Knowledge Network</a></div>");
+                }
+	    });
         }
     };
 });
