0b93dad3f148c33712049dfa21d7a61e6b4bbc6f
Jonathan Yu <jonathan.yu@csiro.au>
2018-10-16 08:06:36 +1100

    initial search extender

---
+++ ckanext/csiro_hub_theme/fanstatic/csirohubtheme_search.js
@@ -0,0 +1,29 @@
+"use strict";
+
+var searchKn = function(eventName) {
+};
+
+
+ckan.module('csirohubtheme_search_kn', function ($) {
+    return {
+        initialize: function () {
+            console.log("I've been initialized for element: ", this.el);
+            console.log("this");
+            console.log(this);
+            console.log("this.options");
+            console.log(this.options);
+        }
+    };
+});
+
+ckan.module('csirohubtheme_search_dap', function ($) {
+    return {
+        initialize: function () {
+            console.log("I've been initialized for element: ", this.el);
+            console.log("this");
+            console.log(this);
+            console.log("this.options");
+            console.log(this.options);
+        }
+    };
+});
