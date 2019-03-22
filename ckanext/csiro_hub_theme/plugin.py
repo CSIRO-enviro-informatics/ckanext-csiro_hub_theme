c1f14dc3fe444c0fc8cbfb7a055d645dc4cc5647
Jonathan Yu <jonathan.yu@csiro.au>
2019-03-22 04:32:33 +0000

    adding app versioning display code

--- ckanext/csiro_hub_theme/plugin.py
+++ ckanext/csiro_hub_theme/plugin.py
@@ -1,9 +1,18 @@
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
+from ckan.common import config
+
+
+def appversion():
+    '''Returns the appversion config value.'''
+
+    value = config.get('ckan.appversion', 'unknown')
+    return value
 
 
 class Csiro_Hub_ThemePlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
+    plugins.implements(plugins.ITemplateHelpers)
 
     # IConfigurer
 
@@ -11,3 +20,13 @@ class Csiro_Hub_ThemePlugin(plugins.SingletonPlugin):
         toolkit.add_template_directory(config_, 'templates')
         toolkit.add_public_directory(config_, 'public')
         toolkit.add_resource('fanstatic', 'csiro_hub_theme')
+
+    def get_helpers(self):
+        '''Register the most_popular_groups() function above as a template
+        helper function.
+
+        '''
+        # Template helper function names should begin with the name of the
+        # extension they belong to, to avoid clashing with functions from
+        # other extensions.
+        return {'appversion': appversion}
